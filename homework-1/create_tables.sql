5c5bf8795c66242df2624d4e985cf3845bca69bb
evg007 <evg007554@yandex.ru>
2023-07-05 13:27:07 +0300

    hw1

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,23 @@
 -- SQL-команды для создания таблиц
+
+CREATE TABLE IF NOT EXISTS public.customer
+(
+    customer_id character varying(5) COLLATE pg_catalog."default" NOT NULL,
+    company_name character varying(250) COLLATE pg_catalog."default",
+    contact_name character varying(250) COLLATE pg_catalog."default",
+    CONSTRAINT customer_pkey PRIMARY KEY (customer_id)
+)
+
+;
+CREATE TABLE IF NOT EXISTS public.employee
+(
+    employee_id integer NOT NULL,
+    first_name character varying(250) COLLATE pg_catalog."default",
+    last_name character varying(250) COLLATE pg_catalog."default",
+    title character varying(250) COLLATE pg_catalog."default",
+    birth_date date,
+    notes text COLLATE pg_catalog."default",
+    CONSTRAINT employee_pkey PRIMARY KEY (employee_id)
+)
+
+;
--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,59 @@
 """Скрипт для заполнения данными таблиц в БД Postgres."""
+import csv
+import os
+
+import psycopg2 as psycopg2
+
+with open('north_data/employees_data.csv') as file:
+    employees = list(csv.reader(file))
+
+with open('north_data/customers_data.csv') as file:
+    customers = list(csv.reader(file))
+
+with open('north_data/orders_data.csv') as file:
+    orders = list(csv.reader(file))
+
+psw= os.getenv('PAS_POSTGSQL')
+psw='xStatus34'
+print(psw)
+
+with psycopg2.connect(
+host = 'localhost',
+port = '5432',
+dbname = 'north',
+user = 'postgres',
+password = psw
+) as connect_north:
+    with connect_north.cursor() as curs:
+        for employee in employees[1:]:
+            s=f'''
+            INSERT INTO employee(employee_id, first_name, last_name, title,  birth_date, notes)
+	        VALUES ({employee[0]},'{employee[1]}','{employee[2]}', '{employee[3]}', '{employee[4]}', '{employee[5]}')
+	        '''
+            # print(s)
+            curs.execute(s)
+
+        with connect_north.cursor() as curs:
+            for customer in customers[1:]:
+                # s = f"""
+                # INSERT INTO public.customer(
+    	        #         customer_id, company_name, contact_name)
+    	        # VALUES ('{customer[0]}','{customer[1]}','{customer[2]}');
+    	        # """
+                # print(s)
+                curs.execute('INSERT INTO public.customer(customer_id, company_name, contact_name) VALUES (%s,%s,%s)',
+                             (customer[0],customer[1],customer[2] ) )
+
+
+
+        with connect_north.cursor() as curs:
+            for order in orders[1:]:
+
+
+                s = f"""
+                INSERT INTO public.customer(
+    	                order_id, customer_id, employee_id, order_date, ship_city)
+    	        VALUES ({order[0]},'{order[1]}',{order[2]},'{order[3]}','{order[4]}')
+    	        """
+                print(s)
+                curs.execute(s)
