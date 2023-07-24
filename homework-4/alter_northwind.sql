c87eebcc4794490d85a21594800b93ab697c5d31
evg007 <evg007554@yandex.ru>
2023-07-24 19:20:52 +0300

    hw4

--- homework-4/alter_northwind.sql
+++ homework-4/alter_northwind.sql
@@ -1,12 +1,51 @@
 -- Подключиться к БД Northwind и сделать следующие изменения:
 -- 1. Добавить ограничение на поле unit_price таблицы products (цена должна быть больше 0)
-
+ALTER TABLE IF EXISTS public.products
+    ADD CONSTRAINT unit_price CHECK (unit_price>0)
+    NOT VALID;
 
 -- 2. Добавить ограничение, что поле discontinued таблицы products может содержать только значения 0 или 1
-
+ALTER TABLE IF EXISTS public.products
+    ADD CONSTRAINT discontinued CHECK (discontinued in (0,1))
+    NOT VALID;
 
 -- 3. Создать новую таблицу, содержащую все продукты, снятые с продажи (discontinued = 1)
 
+CREATE TABLE IF NOT EXISTS public.discontinued_products
+(
+    product_id smallint NOT NULL,
+    product_name character varying(40) COLLATE pg_catalog."default" NOT NULL,
+    supplier_id smallint,
+    category_id smallint,
+    quantity_per_unit character varying(20) COLLATE pg_catalog."default",
+    unit_price real,
+    units_in_stock smallint,
+    units_on_order smallint,
+    reorder_level smallint,
+    discontinued integer NOT NULL,
+    CONSTRAINT pk_discontinued_products PRIMARY KEY (product_id),
+    CONSTRAINT fk_discontinued_products_categories FOREIGN KEY (category_id)
+        REFERENCES public.categories (category_id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION,
+    CONSTRAINT fk_discontinued_products_suppliers FOREIGN KEY (supplier_id)
+        REFERENCES public.suppliers (supplier_id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION,
+    CONSTRAINT unit_price CHECK (unit_price > 0::double precision) NOT VALID,
+    CONSTRAINT discontinued CHECK (discontinued = ANY (ARRAY[0, 1])) NOT VALID
+);
+INSERT INTO discontinued_products
+SELECT * FROM products WHERE discontinued = 1;
 
 -- 4. Удалить из products товары, снятые с продажи (discontinued = 1)
 -- Для 4-го пункта может потребоваться удаление ограничения, связанного с foreign_key. Подумайте, как это можно решить, чтобы связь с таблицей order_details все же осталась.
+DELETE FROM public.order_details
+	WHERE product_id in (select  product_id FROM products WHERE discontinued = 1);
+;
+DELETE
+--select *
+FROM orders
+	WHERE order_id not in (select  order_id FROM order_details );
+;
+DELETE FROM products WHERE discontinued = 1;
--- homework-4/alter_student.sql
+++ homework-4/alter_student.sql
@@ -1,19 +1,44 @@
 -- 1. Создать таблицу student с полями student_id serial, first_name varchar, last_name varchar, birthday date, phone varchar
+CREATE TABLE public.student
+(
+    student_id serial NOT NULL,
+    first_name character varying(100),
+    last_name character varying(100),
+    birthday_date date,
+    phone character varying(100),
+    PRIMARY KEY (student_id)
+);
 
+ALTER TABLE IF EXISTS public.student
+    OWNER to postgres;
 
 -- 2. Добавить в таблицу student колонку middle_name varchar
-
+ALTER TABLE IF EXISTS public.student
+    ADD COLUMN middle_name character varying(100);
 
 -- 3. Удалить колонку middle_name
-
+ALTER TABLE IF EXISTS public.student DROP COLUMN IF EXISTS middle_name;
 
 -- 4. Переименовать колонку birthday в birth_date
-
+ALTER TABLE IF EXISTS public.student
+    RENAME birthday_date TO birth_date;
 
 -- 5. Изменить тип данных колонки phone на varchar(32)
-
+ALTER TABLE public.student
+    ALTER COLUMN phone TYPE character varying(32) COLLATE pg_catalog."default";
 
 -- 6. Вставить три любых записи с автогенерацией идентификатора
+INSERT INTO public.student(
+	 first_name, last_name, birth_date, phone)
+	VALUES ('first_name1', 'last_name1', '1980-01-01', '11111');
 
+INSERT INTO public.student(
+	 first_name, last_name, birth_date, phone)
+	VALUES ('first_name2', 'last_name2', '1980-01-01', '2 222 222 22 22');
+
+INSERT INTO public.student(
+	 first_name, last_name, birth_date, phone)
+	VALUES ('first_name3', 'last_name3', '1980-01-01', '3 333 333 33 33');
 
 -- 7. Удалить все данные из таблицы со сбросом идентификатор в исходное состояние
+TRUNCATE TABLE student RESTART IDENTITY;
